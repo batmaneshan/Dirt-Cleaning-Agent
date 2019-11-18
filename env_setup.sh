7cab45c3f26de8fdffa0b687e3fae40a2426823d
Tithi Gupta <43164922+tvgupta@users.noreply.github.com>
2019-11-17 17:21:08 -0700

    Add files via upload

--- env_setup.sh
+++ env_setup.sh
@@ -1,15 +1,14 @@
 # copy model files
-cp -r ~/catkin_ws/src/dirt_cleaning_agent/helpers/models/* ~/.gazebo/models
+cp -r ./helpers/models/* ~/.gazebo/models
 
 # copy basket mesh files
-cp -r ~/catkin_ws/src/dirt_cleaning_agent/helpers/turtlebot/basket ~/catkin_ws/src/turtlebot3/turtlebot3_description/meshes/
+cp -r ./helpers/turtlebot/basket ~/catkin_ws/src/turtlebot3/turtlebot3_description/meshes/
 
 # copy xacro file
-cp -r ~/catkin_ws/src/dirt_cleaning_agent/helpers/turtlebot/turtlebot3_waffle.urdf.xacro ~/catkin_ws/src/turtlebot3/turtlebot3_description/urdf/
+cp -r ./helpers/turtlebot/turtlebot3_waffle.urdf.xacro ~/catkin_ws/src/turtlebot3/turtlebot3_description/urdf/
 
 # catkin make
 cd ~/catkin_ws && catkin_make
 
 # source latest
 source ~/catkin_ws/devel/setup.bash
-
